dde617a682fa6911d128b119a7976c9933985f9c
Zunayed <zunayedology@gmail.com>
2025-05-13 16:36:16 +0600

    Unit Test passed

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const exprEval = require('expr-eval');
 
+
 function safeEval(expression) {
     try {
         // Check for division by zero
         if (/\/\s*0(?!\d)/.test(expression)) {
             return "Undefined";
+        }
+        if (/[\+\-\*\/]{2,}/.test(expression.replace(/\s+/g, ''))) {
+            return "Error";
         }
         const parser = new exprEval.Parser();
         let result = parser.evaluate(expression);
@@ -13,11 +17,11 @@ function safeEval(expression) {
         }
         return result;
     } catch (e) {
-        // Log the error for debugging purposes
         console.error("Expression evaluation error:", e);
         return "Error";
     }
 }
+
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { safeEval };
